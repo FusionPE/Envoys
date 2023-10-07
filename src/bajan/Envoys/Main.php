2223a63d961dcf6d48d218e7b36af90898954381
HazeDev650 <145964153+HazeDev650@users.noreply.github.com>
2023-10-06 20:53:05 -0700

    Update Main.php

--- src/bajan/Envoys/Main.php
+++ src/bajan/Envoys/Main.php
@@ -84,7 +84,14 @@ class Main extends PluginBase implements Listener {
 
             if ($itemObj instanceof \pocketmine\item\Item) {
                 $world = $sender->getWorld();
-                $world->setBlock($position->asVector3(), new \pocketmine\block\Chest());
+                $nbt = CompoundTag::create()
+                ->setTag("Items", new ListTag([]))
+                ->setString("id", "Chest")
+                ->setInt("x", floor($position->x))
+                ->setInt("y", floor($position->y))
+                ->setInt("z", floor($position->z));
+                $chest = new \pocketmine\block\tile\Chest($world, $nbt);
+                $world->setBlock($position->asVector3(), $chest);
                 $nbt = CompoundTag::create()
                     ->setTag("Items", new ListTag([]))
                     ->setString("id", "Chest")
