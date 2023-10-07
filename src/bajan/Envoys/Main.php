dcdb71257e2cf9e0f8d1b9e35ad6e5e2fda177f6
Spooffyyy <145518793+Spooffyyy@users.noreply.github.com>
2023-10-06 17:46:37 -0700

    Update Main.php

--- src/bajan/Envoys/Main.php
+++ src/bajan/Envoys/Main.php
@@ -7,7 +7,7 @@ use pocketmine\event\Listener;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\utils\Config;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\block\Block;
 use pocketmine\item\Item;
 use pocketmine\utils\TextFormat as TF;
@@ -41,9 +41,9 @@ class Main extends PluginBase implements Listener{
 			$players->sendMessage(TF::AQUA."WORLD EVENT");
 			$players->sendMessage(TF::GREEN."Envoys are being spawned in the warzone!");
 		}
-		foreach($this->envoys as $data => $level){
+		foreach($this->envoys as $data => $world){
 			$data = explode(":",$data);
-			$tile = $this->getServer()->getLevelByName($level)->getTile(new Vector3(intval($data[0]),intval($data[1]),intval($data[2])));
+			$tile = $this->getServer()->getWorldByName($world)->getTile(new Vector3(intval($data[0]),intval($data[1]),intval($data[2])));
 			$i = rand(3,5);
 			while($i > 0){
 				$item = $this->items[array_rand($this->items)];
@@ -55,13 +55,13 @@ class Main extends PluginBase implements Listener{
 	}
 
 	public function setEnvoy(Player $sender){
-		$this->envoys->set($sender->x.":".$sender->y.":".$sender->z, $sender->getLevel()->getName());
+		$this->envoys->set($sender->x.":".$sender->y.":".$sender->z, $sender->getWorld()->getName());
 		$this->envoys->save();
 		$items = $this->items->get("Items");
 		$item = $items[array_rand($items)];
 		$values = explode(":", $item);
-		$level = $sender->getLevel();
-		$level->setBlock($sender->getPosition()->asVector3(), Block::get(54));
+		$world = $sender->getWorld();
+		$world->setBlock($sender->getPosition()->asVector3(), Block::get(54));
 		$nbt = new CompoundTag(" ", [
 			new ListTag("Items", []),
 			new StringTag("id", Tile::CHEST),
@@ -69,8 +69,8 @@ class Main extends PluginBase implements Listener{
 			new IntTag("y", $sender->y),
 			new IntTag("z", $sender->z)
 		]);
-		$chest = Tile::createTile("Chest", $sender->getLevel(), $nbt);
-		$level->addTile($chest);
+		$chest = Tile::createTile("Chest", $sender->getWorld(), $nbt);
+		$world->addTile($chest);
 		$inv = $chest->getInventory();
 		$inv->addItem(Item::get($values[0], $values[1]));
 		$sender->sendMessage(TF::GREEN."Envoy set!");
