5aaf54e88eb72e15c246cc776daed0134fc77845
Spooffyyy <145518793+Spooffyyy@users.noreply.github.com>
2023-10-06 17:50:35 -0700

    Update EnvoyTask.php

--- src/bajan/Envoys/EnvoyTask.php
+++ src/bajan/Envoys/EnvoyTask.php
@@ -7,9 +7,9 @@ use pocketmine\Server;
 class EnvoyTask extends PluginTask {
 
   public function __construct(Main $plugin) {
-		parent::__construct($plugin);
-		$this->plugin = $plugin;
-	}
+    parent::__construct($plugin);
+    $this->plugin = $plugin;
+}
 
   public function onRun($currenttick) {
 		$this->plugin->runEnvoyEvent();
