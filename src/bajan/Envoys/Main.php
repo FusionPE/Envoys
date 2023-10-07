39a20d6483ff928c2132afe1df2f0eb8b7ec0d67
Spooffyyy <145518793+Spooffyyy@users.noreply.github.com>
2023-10-06 17:59:40 -0700

    Update Main.php

--- src/bajan/Envoys/Main.php
+++ src/bajan/Envoys/Main.php
@@ -19,21 +19,30 @@ use pocketmine\nbt\tag\ListTag;
 use pocketmine\nbt\tag\StringTag;
 use pocketmine\tile\Tile;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase implements Listener {
 
-	//minutes
-	public $spawntime = 60;
+    // minutes
+    public $spawntime = 60;
+    
+    /** @var Config */
+    private $cfg;
+    
+    /** @var Config */
+    private $envoys;
+    
+    /** @var Config */
+    private $items;
 
-	public function onEnable(): void {
-    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    $this->getScheduler()->scheduleRepeatingTask(new EnvoyTask($this), $this->spawntime * 60 * 20);
-    @mkdir($this->getDataFolder());
-    $this->saveResource("Config.yml");
-    $this->saveResource("Envoys.yml");
-    $this->saveResource("Items.yml");
-    $this->cfg = new Config($this->getDataFolder() . "Config.yml", Config::YAML);
-    $this->envoys = new Config($this->getDataFolder() . "Envoys.yml", Config::YAML);
-    $this->items = new Config($this->getDataFolder() . "Items.yml", Config::YAML);
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getScheduler()->scheduleRepeatingTask(new EnvoyTask($this), $this->spawntime * 60 * 20);
+        @mkdir($this->getDataFolder());
+        $this->saveResource("Config.yml");
+        $this->saveResource("Envoys.yml");
+        $this->saveResource("Items.yml");
+        $this->cfg = new Config($this->getDataFolder() . "Config.yml", Config::YAML);
+        $this->envoys = new Config($this->getDataFolder() . "Envoys.yml", Config::YAML);
+        $this->items = new Config($this->getDataFolder() . "Items.yml", Config::YAML);
     }
 
 	public function runEnvoyEvent(): void {
