18b42ea696cac77ab57e79df994230d77924a104
Spooffyyy <145518793+Spooffyyy@users.noreply.github.com>
2023-10-06 17:43:16 -0700

    Update EnvoyTask.php

--- src/bajan/Envoys/EnvoyTask.php
+++ src/bajan/Envoys/EnvoyTask.php
@@ -11,7 +11,7 @@ class EnvoyTask extends PluginTask {
 		$this->plugin = $plugin;
 	}
 
-  public function onRun($currenttick){
+  public function onRun(): void {
 		$this->plugin->runEnvoyEvent();
 		return true;
   }
