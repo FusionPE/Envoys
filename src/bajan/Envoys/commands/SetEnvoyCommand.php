b800627efe8377a148275b68dfa26db6976af248
FusionPE <74400210+FusionPE@users.noreply.github.com>
2023-10-07 13:07:05 -0700

    Update SetEnvoyCommand.php

--- src/bajan/Envoys/commands/SetEnvoyCommand.php
+++ src/bajan/Envoys/commands/SetEnvoyCommand.php
@@ -9,8 +9,7 @@ use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\math\Vector3;
-use pocketmine\block\tile\Chest;
-use pocketmine\block\tile\TileFactory;
+use pocketmine\tile\Chest;
 use pocketmine\item\StringToItemParser;
 use pocketmine\item\Item;
 use bajan\Envoys\Main;
@@ -52,11 +51,12 @@ class SetEnvoyCommand extends Command {
 
             if ($itemObj instanceof Item) {
                 $world = $sender->getWorld();
-                $tileFactory = TileFactory::getInstance();
-                $nbt = $tileFactory->createBaseNBT($coords);
-                $chest = Chest::createTile($world, $nbt);
-                $world->addTile($chest);
-                $inv = $chest->getInventory();
+                $tile = new Chest($world->getChunk($coords->x >> 4, $coords->z >> 4), $nbt = null);
+                $tile->x = $coords->x;
+                $tile->y = $coords->y;
+                $tile->z = $coords->z;
+
+                $inv = $tile->getInventory();
                 $inv->addItem($itemObj);
 
                 $sender->sendMessage(TF::GREEN . "Envoy set at $coords in world $worldName!");
