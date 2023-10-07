9535a68fc9daac6a68498a369d4c77f3efb92d01
FusionPE <74400210+FusionPE@users.noreply.github.com>
2023-10-07 12:18:05 -0700

    Update Main.php

--- src/bajan/Envoys/Main.php
+++ src/bajan/Envoys/Main.php
@@ -33,56 +33,67 @@ class Main extends PluginBase implements Listener {
         $this->saveResource("config.yml");
         $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
         $this->spawntime = $config->get("envoy_interval", 60);
-        $this->getScheduler()->scheduleRepeatingTask(new EnvoyTask($this), $this->spawntime * 60 * 20);
         @mkdir($this->getDataFolder());
         $this->saveResource("Items.yml");
         $this->items = new Config($this->getDataFolder() . "Items.yml", Config::YAML);
         $this->envoys = new Config($this->getDataFolder() . "Envoys.yml", Config::YAML);
         $this->getServer()->getCommandMap()->register("setenvoy", new SetEnvoyCommand($this));
     }
-    
+
     public function getItemsConfig(): Config {
         return $this->items;
     }
-    
+
     public function getEnvoysConfig(): Config {
         return $this->envoys;
     }
-    
-    public function runEnvoyEvent(): void {
-        $countdown = 10; // Countdown in seconds before chest spawns
+
+    public function runEnvoyEvent(int $countdownTime): void {
+        $countdown = $countdownTime * 60; // Convert minutes to seconds
+
+        $countdowns = [1200, 900, 600, 300, 60, 30, 10]; // Countdowns in seconds
+        $messages = [
+            "$countdownTime mins",
+            "15 mins",
+            "10 mins",
+            "5 mins",
+            "1 min",
+            "30 sec",
+            "10 sec"
+        ];
 
         foreach ($this->getServer()->getOnlinePlayers() as $player) {
             $player->sendMessage(TF::AQUA . "WORLD EVENT");
-            $player->sendMessage(TF::GREEN . "Envoys are being spawned in the warzone! Spawning in $countdown seconds...");
+            $player->sendMessage(TF::GREEN . "Envoys are being spawned in the warzone!");
+
+            foreach ($countdowns as $index => $count) {
+                if ($count <= $countdown) {
+                    $this->getScheduler()->scheduleDelayedTask(new CountdownTask($this, $player, $count, $messages[$index]), 20);
+                }
+            }
         }
 
-        $this->getScheduler()->scheduleDelayedTask(new EnvoyTask($this), $countdown * 20);
-
         $envoyData = $this->getEnvoysConfig()->getAll();
+
         foreach ($envoyData as $envoy) {
             $coords = explode(":", $envoy["coords"]);
-            $world = $this->getServer()->getWorldManager()->getWorldByName($envoy["world"]);
+            $worldName = $envoy["world"];
+            $world = $this->getServer()->getWorldManager()->getWorldByName($worldName);
             $tile = $world->getTile(new Vector3(intval($coords[0]), intval($coords[1]), intval($coords[2])));
-            $i = rand(3, 5);
 
-            while ($i > 0) {
-                $itemsList = $this->getItemsConfig()->get("Items");
-
-                if (is_array($itemsList)) {
-                    foreach ($itemsList as $itemString) {
-                        $itemObj = StringToItemParser::getInstance()->parse($itemString);
-
-                        if ($itemObj instanceof \pocketmine\item\Item) {
-                            if ($tile instanceof \pocketmine\block\tile\Chest) {
+            if ($countdown <= 0) {
+                if ($tile instanceof \pocketmine\block\tile\Chest) {
+                    $itemsList = $this->getItemsConfig()->get("Items");
+                    if (is_array($itemsList)) {
+                        foreach ($itemsList as $itemString) {
+                            $itemObj = StringToItemParser::getInstance()->parse($itemString);
+                            if ($itemObj instanceof \pocketmine\item\Item) {
                                 $chest = $tile;
                                 $chest->getInventory()->addItem($itemObj);
                             }
                         }
                     }
                 }
-
-                $i--;
             }
         }
     }
