545558f0e9332be238800217af4d72b2bff25157
FusionPE <74400210+FusionPE@users.noreply.github.com>
2023-10-07 13:15:18 -0700

    Update Main.php

--- src/bajan/Envoys/Main.php
+++ src/bajan/Envoys/Main.php
@@ -1,100 +1,88 @@
 <?php
 
-declare(strict_types=1);
+namespace klaus\Envoys;
 
-namespace bajan\Envoys;
-
-use pocketmine\block\VanillaBlocks;
-use pocketmine\block\tile\Tile;
-use pocketmine\item\StringToItemParser;
-use pocketmine\nbt\tag\CompoundTag;
-use pocketmine\nbt\tag\ListTag;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
 use pocketmine\utils\Config;
+use pocketmine\Player;
+use pocketmine\block\Block;
+use pocketmine\item\Item;
 use pocketmine\utils\TextFormat as TF;
-use pocketmine\player\Player;
 use pocketmine\math\Vector3;
-use bajan\Envoys\commands\SetEnvoyCommand;
+use pocketmine\nbt\NBT;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\tile\Tile;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener{
 
-    // minutes
-    public $spawntime = 60;
+	//minutes
+	public $spawntime = 5;
 
-    /** @var Config */
-    private $items;
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getScheduler()->scheduleRepeatingTask(new EnvoyTask($this), $this->spawntime*60*20);
+		@mkdir($this->getDataFolder());
+		$this->saveResource("Config.yml");
+		$this->saveResource("Envoys.yml");
+		$this->saveResource("Items.yml");
+		$this->cfg = new Config($this->getDataFolder()."Config.yml",Config::YAML);
+		$this->envoys = new Config($this->getDataFolder()."Envoys.yml",Config::YAML);
+		$this->items = new Config($this->getDataFolder()."Items.yml",Config::YAML);
+	}
 
-    /** @var Config */
-    private $envoys;
+	public function runEnvoyEvent(){
+		foreach($this->getServer()->getOnlinePlayers() as $players){
+			$players->sendMessage(TF::AQUA."WORLD EVENT");
+			$players->sendMessage(TF::GREEN."Envoys are being spawned in the warzone!");
+		}
+		foreach($this->envoys as $data => $level){
+			$data = explode(":",$data);
+			$tile = $this->getServer()->getLevelByName($level)->getTile(new Vector3(intval($data[0]),intval($data[1]),intval($data[2])));
+			$i = rand(3,5);
+			while($i > 0){
+				$item = $this->items[array_rand($this->items)];
+				$item = explode(":",$item);
+				$tile->getInventory()->addItem(Item::get($item[0],$item[1],$item[2]));
+				$i--;
+			}
+		}
+	}
 
-    public function onEnable(): void {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->saveResource("config.yml");
-        $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-        $this->spawntime = $config->get("envoy_interval", 60);
-        @mkdir($this->getDataFolder());
-        $this->saveResource("Items.yml");
-        $this->items = new Config($this->getDataFolder() . "Items.yml", Config::YAML);
-        $this->envoys = new Config($this->getDataFolder() . "Envoys.yml", Config::YAML);
-        $this->getServer()->getCommandMap()->register("setenvoy", new SetEnvoyCommand($this));
-    }
+	public function setEnvoy(Player $sender){
+		$this->envoys->set($sender->x.":".$sender->y.":".$sender->z, $sender->getLevel()->getName());
+		$this->envoys->save();
+		$items = $this->items->get("Items");
+		$item = $items[array_rand($items)];
+		$values = explode(":", $item);
+		$level = $sender->getLevel();
+		$level->setBlock($sender->getPosition()->asVector3(), Block::get(54));
+		$nbt = new CompoundTag(" ", [
+			new ListTag("Items", []),
+			new StringTag("id", Tile::CHEST),
+			new IntTag("x", $sender->x),
+			new IntTag("y", $sender->y),
+			new IntTag("z", $sender->z)
+		]);
+		$chest = Tile::createTile("Chest", $sender->getLevel(), $nbt);
+		$level->addTile($chest);
+		$inv = $chest->getInventory();
+		$inv->addItem(Item::get($values[0], $values[1]));
+		$sender->sendMessage(TF::GREEN."Envoy set!");
+		return true;
+	}
 
-    public function getItemsConfig(): Config {
-        return $this->items;
-    }
-
-    public function getEnvoysConfig(): Config {
-        return $this->envoys;
-    }
-
-    public function runEnvoyEvent(int $countdownTime): void {
-        $countdown = $countdownTime * 60; // Convert minutes to seconds
-
-        $countdowns = [1200, 900, 600, 300, 60, 30, 10]; // Countdowns in seconds
-        $messages = [
-            "$countdownTime mins",
-            "15 mins",
-            "10 mins",
-            "5 mins",
-            "1 min",
-            "30 sec",
-            "10 sec"
-        ];
-
-        foreach ($this->getServer()->getOnlinePlayers() as $player) {
-            $player->sendMessage(TF::AQUA . "WORLD EVENT");
-            $player->sendMessage(TF::GREEN . "Envoys are being spawned in the warzone!");
-
-            foreach ($countdowns as $index => $count) {
-                if ($count <= $countdown) {
-                    $this->getScheduler()->scheduleDelayedTask(new CountdownTask($this, $player, $count, $messages[$index]), 20);
-                }
-            }
-        }
-
-        $envoyData = $this->getEnvoysConfig()->getAll();
-
-        foreach ($envoyData as $envoy) {
-            $coords = explode(":", $envoy["coords"]);
-            $worldName = $envoy["world"];
-            $world = $this->getServer()->getWorldManager()->getWorldByName($worldName);
-            $tile = $world->getTile(new Vector3(intval($coords[0]), intval($coords[1]), intval($coords[2])));
-
-            if ($countdown <= 0) {
-                if ($tile instanceof \pocketmine\block\tile\Chest) {
-                    $itemsList = $this->getItemsConfig()->get("Items");
-                    if (is_array($itemsList)) {
-                        foreach ($itemsList as $itemString) {
-                            $itemObj = StringToItemParser::getInstance()->parse($itemString);
-                            if ($itemObj instanceof \pocketmine\item\Item) {
-                                $chest = $tile;
-                                $chest->getInventory()->addItem($itemObj);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+		switch($cmd){
+			case "setenvoy":
+				if(!$sender->hasPermission("envoy.set")) {$sender->sendMessage(TF::RED."You do not have the required permission"); return false;}
+				$this->setEnvoy($sender);
+				return true;
+		}
+	}
 }
