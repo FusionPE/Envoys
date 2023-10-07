becb68238b2fcf6889c8bc0f231d6956b74891e6
HazeDev650 <145964153+HazeDev650@users.noreply.github.com>
2023-10-06 22:07:52 -0700

    Update SetEnvoyCommand.php

--- src/bajan/Envoys/commands/SetEnvoyCommand.php
+++ src/bajan/Envoys/commands/SetEnvoyCommand.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace bajan\Envoys\commands;
 
-use pocketmine\player\Player;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
 use pocketmine\utils\TextFormat as TF;
 
 class SetEnvoyCommand extends Command {
@@ -14,25 +14,28 @@ class SetEnvoyCommand extends Command {
     /** @var \bajan\Envoys\Main */
     private $plugin;
 
-    public function __construct(\bajan\Envoys\Main $plugin) {
+    /** @var \pocketmine\utils\Config */
+    private $envoys;
+
+    public function __construct(\bajan\Envoys\Main $plugin, \pocketmine\utils\Config $envoys) {
         parent::__construct("setenvoy", "Set an envoy");
         $this->plugin = $plugin;
+        $this->envoys = $envoys;
         $this->setPermission("envoys.cmd");
     }
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-    if ($command->getName() === "setconvoy" && $sender instanceof Player) {
-        $coords = floor($sender->x) . ":" . floor($sender->y) . ":" . floor($sender->z) . ":" . $sender->getWorld()->getFolderName();
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if ($sender instanceof Player) {
+            $coords = floor($sender->x) . ":" . floor($sender->y) . ":" . floor($sender->z) . ":" . $sender->getWorld()->getFolderName();
 
-        $envoyData = $this->envoys->getAll();
-        $envoyData["envoy" . count($envoyData) + 1] = $coords;
-        $this->envoys->setAll($envoyData);
-        $this->envoys->save();
+            $envoyData = $this->envoys->getAll();
+            $envoyData["envoy" . (count($envoyData) + 1)] = $coords;
+            $this->envoys->setAll($envoyData);
+            $this->envoys->save();
 
-        $sender->sendMessage(TF::GREEN . "Envoy set at $coords!");
-        return true;
+            $sender->sendMessage(TF::GREEN . "Envoy set at $coords!");
+            return true;
         }
-    return false;
+        return false;
     }
-
 }
